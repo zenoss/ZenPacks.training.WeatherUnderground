359cd0fe9e0b8349d0c220358ced88efdbc7d13d
Chet Luther <chet.luther@gmail.com>
2014-06-16 10:52:59 -0500

    Add ZenPackSpec and Locations modeler plugin.

---
+++ ZenPacks/training/WeatherUnderground/__init__.py
@@ -0,0 +1,55 @@
+from . import zenpacklib
+
+
+CFG = zenpacklib.ZenPackSpec(
+    name=__name__,
+
+    zProperties={
+        'DEFAULTS': {'category': 'Weather Underground'},
+
+        'zWundergroundAPIKey': {
+            'type': 'string',
+            'default': '',
+        },
+
+        'zWundergroundLocations': {
+            'type': 'lines',
+            'default': ['Austin, TX', 'San Jose, CA', 'Annapolis, MD'],
+        },
+    },
+
+    classes={
+        'WundergroundDevice': {
+            'base': zenpacklib.Device,
+            'label': 'Weather Underground API',
+        },
+
+        'WundergroundLocation': {
+            'base': zenpacklib.Component,
+            'label': 'Location',
+            'properties': {
+                'country_code': {
+                    'label': 'Country Code',
+                    'order': 4.0,
+                },
+
+                'timezone': {
+                    'label': 'Time Zone',
+                    'order': 4.1,
+                },
+
+                'api_link': {
+                    'label': 'API Link',
+                    'order': 4.9,
+                    'grid_display': False,
+                },
+            }
+        },
+    },
+
+    class_relationships=zenpacklib.relationships_from_yuml(
+        """[WundergroundDevice]++-[WundergroundLocation]"""
+        )
+)
+
+CFG.create()
